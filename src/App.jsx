6c4342df5124aff7255587fb9914a9356f0da626
wangpeng <wangpeng@yunionyun.com>
2019-06-07 18:12:55 +0800

    init-5: router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import styled from "styled-components";
+import Dashboard from "./views/dashboard";
+import Users from "./views/users";
 
 const Wrapper = styled.section`
   padding: 5px;
@@ -16,6 +19,16 @@ function App() {
     <Wrapper>
       <Title>title</Title>
       <div>Hello world</div>
+      <Router>
+        <Link to="/">
+          <button>dashboard</button>
+        </Link>
+        <Link to="/users">
+          <button>users</button>
+        </Link>
+        <Route path="/" name="Dashboard" exact component={Dashboard} />
+        <Route path="/users" name="Users" component={Users} />
+      </Router>
     </Wrapper>
   );
 }
